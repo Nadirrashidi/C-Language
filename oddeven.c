a50cf7c02bcacc04bbf8e94983666294b3f1f729
Nadirrashidi <111681837+Nadirrashidi@users.noreply.github.com>
2024-02-08 18:28:33 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,17 @@
+// odd or even
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the value of n\n");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+			printf("number is even\n");
+		printf("abhishek");
+	}
+	else
+	{
+		printf("number is odd");
+	}
+}
